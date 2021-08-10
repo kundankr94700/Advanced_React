7d05b508048cf997e125c73ff0748414f32305a4
Kundan Kumar <kundankr94700@gmail.com>
2021-08-10 17:57:49 +0530

    UseState Example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import UseStateBasic from "./Tutorial/UseStateBasic";
-// import UseStateExample from "./Tutorial/useStateExample";
+import UseStateBasic from "./Tutorial/UseStateBasic"; import UseStateExample from "./Tutorial/useStateExample";
 function App() {
   return (
     <div className="container">
       <header className="App-header">
       <h1>Advance React</h1>
-        {/* <UseStateExample/> */}
-        <UseStateBasic/>
+        <UseStateExample/>
+        {/* <UseStateBasic/> */}
       </header>
     </div>
   );
--- src/Tutorial/useStateExample.js
+++ src/Tutorial/useStateExample.js
@@ -2,13 +2,15 @@ import React,{useState} from 'react'
 
 function UseStateExample() {
     let title="random title";
-    const handleClick=()=>{
-        console.log(title)
+    const handleClick=(b)=>{
+        let c=b;
+        //console.log(title)
+        console.log(b);
     }
     return (
         <div>
             <h2>{title}</h2>
-            <button type='button' className='btn' onClick={handleClick}>Change Title</button>
+            <button type='button' className='btn' onClick={handleClick(80000)}>Change Title</button>
         </div>
     )
 }
