55c51abc1822022447d9a5f8314ecda8fc36fc77
Kundan Kumar <kundankr94700@gmail.com>
2021-08-11 09:55:24 +0530

    usestatearray

---
+++ src/Tutorial/Data.js
@@ -0,0 +1,6 @@
+export const data=[
+    {id:1,name:'Kundan Kumar' },
+    {id:2,name:'Rohit Kumar' },
+    {id:3,name:'Rahul Kumar' },
+    {id:4,name:'Rishab Kumar' },
+]
--- src/Tutorial/UseStateArray.js
+++ src/Tutorial/UseStateArray.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {data} from './Tuitorial/Data.js'
 
 function UseStateArray() {
+    const [people,setPeople]=React.useState(data)
+    
     return (
         <div>
+            {
+                people.map(person=>{
+                    console.log(person);
+                    return "hello my friend I am Learning about the avengers";})
+            }
             <h2>Use State is in action</h2>
         </div>
     )
