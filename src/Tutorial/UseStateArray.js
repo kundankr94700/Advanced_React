dae9f1c84475352c5532c9133da23843e66f9362
Kundan Kumar <kundankr94700@gmail.com>
2021-08-11 10:18:16 +0530

    filter object in useState and deleting the specific item

--- src/Tutorial/UseStateArray.js
+++ src/Tutorial/UseStateArray.js
@@ -1,18 +1,30 @@
 import React from 'react'
-import {data} from './Tuitorial/Data.js'
+import {data} from './Data.js'
 
 function UseStateArray() {
     const [people,setPeople]=React.useState(data)
-    
+    const removeItem=(id)=> {
+        let newPeople=people.filter(person=> person.id!==id)
+        setPeople(newPeople)
+    }
     return (
-        <div>
+        <>
             {
-                people.map(person=>{
-                    console.log(person);
-                    return "hello my friend I am Learning about the avengers";})
+               people.map(person=>{
+                   const {id,name}=person;
+                   return(
+                       <div key={id} classname='item'>
+                           <h4>{name}</h4>
+                           <button onClick={()=>removeItem(id)}>Remove him</button>
+                       </div>
+
+                   );
+               })
+                    
             }
             <h2>Use State is in action</h2>
-        </div>
+            <button className='btn' onClick={()=>setPeople([])}>Clear Name</button>
+        </>
     )
 }
 
